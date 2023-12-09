8329bc93735918dff04bcc32675c9b83b2e4cbde
Andre Ribau <Ribauandre@yahoo.com>
2023-12-09 17:09:06 -0500

    advent of code solution day 1 pt1 & pt2 2023

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,92 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var finalCalibrationNumber = 0
+        for (line in input) {
+            val stringBuilder = StringBuilder()
+            val digitCalibrationCode = line.filter { it.isDigit() }
+            if (digitCalibrationCode.length == 1) {
+                var copy = digitCalibrationCode
+
+                stringBuilder.append(copy).append(digitCalibrationCode)
+                var recalibratedCode = stringBuilder.toString()
+
+                finalCalibrationNumber += recalibratedCode.toInt()
+            }
+            else if (digitCalibrationCode.length == 2){
+                finalCalibrationNumber += digitCalibrationCode.toInt()
+            }
+            else {
+                var firstNum = digitCalibrationCode.first()
+                var lastNum = digitCalibrationCode.last()
+
+                stringBuilder.append(firstNum).append(lastNum)
+                var recalibratedCode = stringBuilder.toString()
+
+                finalCalibrationNumber += recalibratedCode.toInt()
+            }
+
+        }
+
+
+        return finalCalibrationNumber
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val wordOfNumber = mapOf("one" to 1, "two" to 2, "three" to 3, "four" to 4, "five" to 5, "six" to 6, "seven" to 7, "eight" to 8, "nine" to 9)
+        var finalCalibrationNumber: MutableList<Int> = mutableListOf<Int>()
+        for (line in input) {
+            var readLine = line
+            val stringBuilder = StringBuilder()
+
+            var wordBuilder = ""
+            var index = 0
+            for (char in line) {
+                index++
+                if(char.isLetter()) {
+                    wordBuilder += char
+                }
+                else {
+                    wordBuilder = char.toString()
+                }
+                for (key in wordOfNumber.keys) {
+                    if (wordBuilder.contains(key)) {
+                        readLine = readLine.replaceRange(index, index, wordOfNumber[key].toString())
+                        readLine = readLine.replaceRange(index+1, index+1, key)
+                        index += key.length+1
+                        wordBuilder = char.toString()
+                    }
+                }
+            }
+
+            // re-used from part 1
+            val digitCalibrationCode = readLine.filter { it.isDigit() }
+
+            if (digitCalibrationCode.length == 1) {
+                var copy = digitCalibrationCode
+                stringBuilder.append(copy).append(digitCalibrationCode)
+                var recalibratedCode = stringBuilder.toString()
+                finalCalibrationNumber.add(recalibratedCode.toInt())
+            }
+            else if (digitCalibrationCode.length == 2){
+                finalCalibrationNumber.add(digitCalibrationCode.toInt())
+            }
+            else {
+                var firstNum = digitCalibrationCode.first()
+                var lastNum = digitCalibrationCode.last()
+                stringBuilder.append(firstNum).append(lastNum)
+                var recalibratedCode = stringBuilder.toString()
+
+                finalCalibrationNumber.add(recalibratedCode.toInt())
+            }
+        }
+        return finalCalibrationNumber.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("the answer for pt 1 is: " + part1(input))
+    println("the answer for pt 2 is: " + part2(input))
 }
